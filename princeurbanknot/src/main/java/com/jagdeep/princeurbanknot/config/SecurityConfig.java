1a4832fe037e95d817c12f9ff39bda79629c304d
Jagdeep Kainth <jagdeep3644kainth@gmail.com>
2026-03-24 21:41:09 -0400

    allowed fronted to fetch product images using API

--- princeurbanknot/src/main/java/com/jagdeep/princeurbanknot/config/SecurityConfig.java
+++ princeurbanknot/src/main/java/com/jagdeep/princeurbanknot/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/products").permitAll()
+                        .requestMatchers("/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
